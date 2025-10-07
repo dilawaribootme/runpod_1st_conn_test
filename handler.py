286ddeb57b6453a9f12354dd4adbbfd70f5dc687
Muhammad Rizwan <165241909+MRizwanMalik@users.noreply.github.com>
2025-10-07 18:23:16 +0500

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,8 +1,9 @@
 import runpod
 
 def handler(job):
-    job_input = job.get("input", {})
+    job_input = job.get["input"]
     name = job_input.get("name", "World")
     return f"Hello, {name}! How are you doing today?"
 
 runpod.serverless.start({"handler": handler})
+
